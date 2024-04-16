84e97039883e78853798a9acb623ee145bcc23f0
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-04-16 20:08:31 +0530

    Create 590. N-ary Tree Postorder Traversal.java

---
+++ Trees/590. N-ary Tree Postorder Traversal.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> res=new ArrayList<>();
+    public List<Integer> postorder(Node root) {
+       helper(root);
+       return res; 
+    }
+    private void helper(Node root){
+        if(root==null){
+            return;
+        }
+        for(Node child:root.children){
+           helper(child); 
+        }
+
+        res.add(root.val);
+    }
+}
