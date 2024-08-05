293ad1d9af01416f4898d638893c34cbcbeec7f3
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-08-05 23:38:38 +0530

    Create 252.Meeting Schedule.java

---
+++ Intervals/252.Meeting Schedule.java
@@ -0,0 +1,56 @@
+/*
+
+Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
+
+Example 1:
+
+Input: intervals = [(0,30),(5,10),(15,20)]
+
+Output: false
+Explanation:
+
+(0,30) and (5,10) will conflict
+(0,30) and (15,20) will conflict
+Example 2:
+
+Input: intervals = [(5,8),(9,15)]
+
+Output: true
+Note:
+
+(0,8),(8,10) is not considered a conflict at 8
+Constraints:
+
+0 <= intervals.length <= 500
+0 <= intervals[i].start < intervals[i].end <= 1,000,000
+
+*/
+
+
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+         Collections.sort(intervals, Comparator.comparingInt(i -> i.start));
+
+         for(int i=1;i<intervals.size();i++){
+            Interval left_end=intervals.get(i-1);
+            Interval right_start=intervals.get(i);
+
+            if(left_end.end>right_start.start){
+                return false;
+            }
+         }
+
+         return true;
+    }
+}
